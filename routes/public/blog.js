c485e1421e815976d02c140a184794762b754f5a
trokhymenko <jura.salg@gmail.com>
2018-08-19 14:00:31 +0300

    show posts list

--- routes/public/blog.js
+++ routes/public/blog.js
@@ -1,10 +1,27 @@
-
+var Posts = require('../../models/posts')
 /** 
  *  GET Blog page
  */
 function getAllPosts(req, res, next) {
-    
-	res.render('public/posts', { title: 'Blog', messages: req.flash()});
+    let perPage = 5;
+    let page = req.query.page || 1;
+    let skip = (perPage * page) - perPage;
+
+    Posts.find({}, null, {skip: skip, limit: perPage}, function(err, posts) {
+        var postsMap = [];
+        posts.forEach(function(post) {
+            postsMap.push({
+              'id': post._id,
+              'title': post.title,
+              'link': '#', /** @TODO: Output link to post */
+              'create_date': '#', /** @TODO: Output date of creation */
+          });
+        });
+
+        Posts.count(function(err, count) {
+            res.render('public/posts', { title: 'Blog', posts: postsMap, current: page, pages: Math.ceil(count / perPage), messages: req.flash() }); 
+        });
+    });   
 }
 
 /**
